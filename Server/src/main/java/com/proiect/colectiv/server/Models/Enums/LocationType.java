0390376b25af77dccdd7e6a521e6cd69bf158f1e
Alexandru Cozma <alexcozma1112@gmail.com>
2025-01-15 08:32:45 +0200

    location type changes

--- Server/src/main/java/com/proiect/colectiv/server/Models/Enums/LocationType.java
+++ Server/src/main/java/com/proiect/colectiv/server/Models/Enums/LocationType.java
@@ -4,8 +4,33 @@ package com.proiect.colectiv.server.Models.Enums;
  * Enum for the location type
  */
 public enum LocationType {
-    FIELD,
-    BARN,
+    // Buildings
+    HOUSE,
+    TRACTOR_SHED,
+    COW_BARN,
+    CHICKEN_COOP,
+    PIG_PEN,
     STORAGE,
-    TOOLS,
+    OTHER,
+
+    // Fields
+    GRASS_FIELD,
+    WHEAT_FIELD,
+    CORN_FIELD,
+    SOYBEANS_FIELD,
+    POTATOES_FIELD,
+    VEGETABLES_FIELD,
+    EMPTY_FIELD,
+
+    // Ponds
+    FISHING_POND,
+    IRRIGATION_POND,
+    DECORATIVE_POND,
+    NATURAL_POND,
+
+    // Roads
+    MAIN_ROAD,
+    ACCESS_PATH,
+    FARM_TRACK,
+    SERVICE_ROAD
 }
